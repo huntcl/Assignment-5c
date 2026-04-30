9c63b820f5be29c2a29843ee055551168a58e3e6
huntcl <huntcl@oregonstate.edu>
2026-04-29 19:42:09 -0700

    Update without_duplicates.py

--- without_duplicates.py
+++ without_duplicates.py
@@ -7,7 +7,7 @@ def without_duplicates(values):
     unique_values = []
 
     for value in values:
-        print("checking:", value)
+        print(value)
         if value not in unique_values:
             unique_values.append(value)
 
